dd384c41b663fd11681bd3867c2902070213b4e3
troqphuc147 <phuchuynh020601@gmail.com>
2022-04-11 19:49:20 +0700

    [FTR] add constraint when spinning

--- app/src/main/java/uit/itszoo/izrandom/random_module/lucky_wheel/LuckyWheelActivity.java
+++ app/src/main/java/uit/itszoo/izrandom/random_module/lucky_wheel/LuckyWheelActivity.java
@@ -4,9 +4,11 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,10 +24,10 @@ import com.bluehomestudio.luckywheel.WheelItem;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import uit.itszoo.izrandom.R;
-import uit.itszoo.izrandom.random_module.random_direction.RandomDirectionActivity;
-import uit.itszoo.izrandom.random_module.random_direction.random_direction_custom.RandomDirectionCustomActivity;
 
 public class LuckyWheelActivity extends AppCompatActivity {
     LuckyWheel lkWheel;
@@ -35,10 +37,12 @@ public class LuckyWheelActivity extends AppCompatActivity {
     ConstraintLayout constrainedLayout;
     ImageButton backButton ;
     ImageButton customButton;
+    private int spinTime = 9000;
     private  String result = "";
-    final int SWIPE_DISTANCE_THRESHOLD = 100;
-    float x1, x2, y1, y2, dx, dy;
-    int  selectedIndex;
+    private final int SWIPE_DISTANCE_THRESHOLD = 100;
+    private float x1, x2, y1, y2, dx, dy;
+    private int  selectedIndex;
+    private boolean spin = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,7 +59,12 @@ public class LuckyWheelActivity extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onBackPressed();
+                if(spin == true)
+                {
+                    Toast.makeText(LuckyWheelActivity.this, "During a spindling", Toast.LENGTH_LONG).show();
+                }
+                else
+                    onBackPressed();
             }
         });
 
@@ -80,21 +89,23 @@ public class LuckyWheelActivity extends AppCompatActivity {
                                 y2 = event.getY();
                                 dx = x2 - x1;
                                 dy = y2 - y1;
-                                if ( Math.abs(dx) > Math.abs(dy) ) {
+                                if ( Math.abs(dx) > Math.abs(dy) && spin == false ) {
                                     if ( dx < 0 && Math.abs(dx) > SWIPE_DISTANCE_THRESHOLD )
                                     {
+                                        spin = true;
                                         Random random = new Random();
-                                        selectedIndex = random.nextInt(wheelItems.size()-1);
+                                        selectedIndex = random.nextInt(wheelItems.size());
                                         lkWheel.setTarget(selectedIndex+1);
                                         result = wheelItems.get(selectedIndex).text;
                                         lkWheel.rotateWheelTo(selectedIndex+1);
                                         description.setVisibility(View.INVISIBLE);
                                     }
                                 } else {
-                                    if ( dy > 0 && Math.abs(dy) > SWIPE_DISTANCE_THRESHOLD )
+                                    if ( dy > 0 && Math.abs(dy) > SWIPE_DISTANCE_THRESHOLD && spin == false)
                                     {
+                                        spin = true;
                                         Random random = new Random();
-                                        selectedIndex = random.nextInt(wheelItems.size()-1);
+                                        selectedIndex = random.nextInt(wheelItems.size());
                                         lkWheel.setTarget(selectedIndex+1);
                                         result = wheelItems.get(selectedIndex).text;
                                         lkWheel.rotateWheelTo(selectedIndex+1);
@@ -125,8 +136,19 @@ public class LuckyWheelActivity extends AppCompatActivity {
                 backButton.setBackgroundColor(wheelItems.get(selectedIndex ).color);
                 customButton.setBackgroundColor(wheelItems.get(selectedIndex ).color);
                 description.setVisibility(View.VISIBLE);
+                spin = false;
             }
         });
+    }
+    private  void setAnimationForBackground(int spinTime)
+    {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        }, spinTime/wheelItems.size());
     }
     private void generateWheelItems() {
         wheelItems = new ArrayList<>();
